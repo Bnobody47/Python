a537eb3e4348052c15b57b617ecca61f7e080028
Bnobody47 <bnobodygetmoney@gmail.com>
2024-06-04 19:45:09 +0300

    new

--- Application 2 WebMaps with Py and Folium/map1.py
+++ Application 2 WebMaps with Py and Folium/map1.py
@@ -1,3 +1,6 @@
 import folium
-map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
+map = folium.Map(location=[9.023922774520175, 38.84411708394214], zoom_start=6, tiles="Mapbox Bright")
+
+map.add_child(folium.Marker())
+
 map.save("Map1.html")
