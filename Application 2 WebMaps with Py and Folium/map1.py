6b70ba9c56aa4f17880e6c85b750600c164965c6
Bnobody47 <bnobodygetmoney@gmail.com>
2024-06-04 20:36:07 +0300

    update

--- Application 2 WebMaps with Py and Folium/map1.py
+++ Application 2 WebMaps with Py and Folium/map1.py
@@ -1,6 +1,9 @@
 import folium
-map = folium.Map(location=[9.023922774520175, 38.84411708394214], zoom_start=6, tiles="Mapbox Bright")
+map = folium.Map(location=[9.023, 38.84], zoom_start=6, titles="Mapbox Bright")
 
-map.add_child(folium.Marker())
+fg = folium.FeatureGroup(name="My Map")
+for coordinates in [[9.023, 38.84], [9.023922774520175, 38.84411708394214]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+map.add_child(fg)
 
 map.save("Map1.html")
