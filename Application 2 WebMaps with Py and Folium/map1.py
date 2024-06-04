3230cc10f6b65406ae8b002437ac4dfb27313015
Bnobody47 <bnobodygetmoney@gmail.com>
2024-06-04 20:46:28 +0300

    popup

--- Application 2 WebMaps with Py and Folium/map1.py
+++ Application 2 WebMaps with Py and Folium/map1.py
@@ -1,9 +1,15 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes_USA.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
 map = folium.Map(location=[9.023, 38.84], zoom_start=6, titles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
-for coordinates in [[9.023, 38.84], [9.023922774520175, 38.84411708394214]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color='green')))
 map.add_child(fg)
 
 map.save("Map1.html")
