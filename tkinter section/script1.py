55ac3d19396cb9251df3a1b23613172bbe0b2fad
Bnobody47 <bnobodygetmoney@gmail.com>
2024-06-07 20:50:08 +0300

    convert

--- tkinter section/script1.py
+++ tkinter section/script1.py
@@ -7,7 +7,7 @@ def km_to_miles():
     miles=float(e1_value.get())*1.6
     t1.insert(END,miles)
 
-b1=Button(window,text="Execute",command=km_to_miles)
+b1=Button(window,text="Convert",command=km_to_miles)
 b1.grid(row=0,column=0)
 
 
