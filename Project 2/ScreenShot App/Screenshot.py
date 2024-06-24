d8457708861915ed8e767a1c8da0c1f9d72dddf1
Bnobody47 <bnobodygetmoney@gmail.com>
2024-06-24 19:26:34 +0300

    Screenshot

---
+++ Project 2/ScreenShot App/Screenshot.py
@@ -0,0 +1,29 @@
+import time
+import pyautogui
+import tkinter as tk
+
+def screenshot():
+    name = int(round(time.time() * 1000))
+    name = 'C:\Users\Bnobody_47\Pictures/{}.png'.forma(name)
+    time.sleep(5)
+    img = pyautogui.screenshot(name)
+    img.show
+
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(
+    frame,
+    text="Screenshot",
+    command=screenshot)
+
+button.pack(side=tk.LEFT)
+Close = tk.Button(
+    frame,
+    text="QUIT",
+    command=squit)
+Close.pack(side=tk.LEFT)
+
+root.mainloop()
