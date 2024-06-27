548c315bf2f1a46a3c156195c0790a3344af18a1
Bnobody47 <bnobodygetmoney@gmail.com>
2024-06-27 15:00:36 +0300

    nnn

--- Project 1/Application 6  Building a Webcam Motion Detector/motion_detector.py
+++ Project 1/Application 6  Building a Webcam Motion Detector/motion_detector.py
@@ -12,7 +12,7 @@ while True:
     check, frame = video.read()
     status=0
 
-    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2BGRAY)
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     gray=cv2.GaussianBlur(gray,(21,21),0)
 
     if first_frame is None:
