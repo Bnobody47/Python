7d47fbd45a9447a64ed6891c3051fa40347889cf
Bnobody47 <bnobodygetmoney@gmail.com>
2024-06-27 17:49:47 +0300

    Speech To Text

---
+++ Project 2/Speech To Text/STT.py
@@ -0,0 +1,20 @@
+#First  pip install SpeechRecognition and pip install pyaudio
+
+import pyttsx3
+import speech_recognition
+
+def get():
+    r = speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        print('done!')
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+    except Exception as e:
+        print(e)
+
+get()
